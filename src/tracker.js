f6de80c7b85ceef3991f5d1dc5e55418bf5e40a1
Alexander Varwijk <git@alexandervarwijk.com>
2020-01-02 11:12:26 +0100

    Add initial payload creation and beacon tracking
    This adds the initial creation of our tracking payload and implements
    submission using the beacon API. A fallback will still be needed for
    browsers that don't support this API.

--- src/tracker.js
+++ src/tracker.js
@@ -38,6 +38,30 @@ function parseUrlParams(params) {
   }
 }
 
+/**
+ * Returns a time(stamp) in milliseconds.
+ *
+ * @param since
+ *   A timestamp that can be used to calculate the difference.
+ * @return {number}
+ *   The number of milliseconds offset or since the unix epoch.
+ */
+function time(since = 0) {
+  return Date.now() - since;
+}
+
+/**
+ * Returns a time(stamp) in seconds.
+ *
+ * @param since
+ *   A timestamp that can be used to calculate the difference.
+ * @return {number}
+ *   The number of seconds offset or since the unix epoch.
+ */
+function timeInSeconds(since = 0) {
+  return Math.round(time(since) / 1000);
+}
+
 function tracker(window, endpoint) {
   // If we're not running in a browser there's nothing to be done.
   if (!window) return;
@@ -73,6 +97,46 @@ function tracker(window, endpoint) {
   try {
     timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   } catch (e) {}
+
+  // Start building a payload with metadata for this request.
+  let payload = {
+    hostname: window.location.hostname,
+    timezone,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    source: {
+      source: query.get('source'),
+      medium: query.get('medium'),
+      campaign: query.get('campaign'),
+      referrer:
+        (window.document.referrer || "")
+          // Strip out the protocol, loadbalancer or mobile specific
+          // subdomains and remove the query string.
+          .replace(
+            /^https?:\/\/((m|l|w{2,3}([0-9]+)?)\.)?([^?#]+)(.*)$/,
+            "$4"
+          )
+          // Normalise to no trailing slash.
+          .replace(/^([^\/]+)\/$/, "$1") || undefined
+    },
+    pageviews: [],
+  };
+
+  // Safari on iOS < 13 has issues with the Beacon API so we don't use it there.
+  const useSendBeacon =
+    typeof window.navigator.sendBeacon !== "undefined" &&
+    /ip(hone|ad)(.*)os\s([1-9]|1[0-2])_/i.test(window.navigator.userAgent) === false;
+
+  if (useSendBeacon) {
+    window.addEventListener("unload", () => {
+      // Timestamp our payload.
+      payload.time = timeInSeconds();
+
+      // Use the beacon API to submit our data to ensure navigation isn't
+      // blocked while we make our request.
+      window.navigator.sendBeacon(endpoint + "post", JSON.stringify(payload));
+    }, false);
+  }
 }
 
 export default tracker;
