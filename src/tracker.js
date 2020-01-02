9ea7d6105e75e4904518c8f2d73beb17e7f0f351
Alexander Varwijk <git@alexandervarwijk.com>
2020-01-02 10:00:15 +0100

    Add tracking constraints
    A simple debugging function is constructed that won't cause script
    errors if the logging is not available. This function is then used to
    exclude tracking in some scenario's. For example, in local development
    or when a user asks us not to be tracked.

--- src/tracker.js
+++ src/tracker.js
@@ -1,6 +1,26 @@
 function tracker(window, endpoint) {
   // If we're not running in a browser there's nothing to be done.
   if (!window) return;
+
+  // Make a simple function that won't cause errors if the console is not
+  // available.
+  const warn = console && console.warn ? message => console.warn("Analytics: " + message) : _message => null;
+
+  // Don't track when the user requests us not to do so.
+  if (typeof nav.doNotTrack !== "undefined" && nav.doNotTrack === "1") {
+    return warn("Not tracking request due to do not track request");
+  }
+
+  // Don't track on localhost.
+  if (window.location.hostname === "localhost" || window.location.protocol === "file:") {
+    return warn("Not tracking request from localhost ");
+  }
+
+  // Filter out any bots we're already sure about before sending them to our
+  // tracking server.
+  if (!nav.userAgent || nav.userAgent.search(/(bot|spider|crawl)/gi) > -1) {
+    return warn("Not tracking request from bots");
+  }
 }
 
 export default tracker;
