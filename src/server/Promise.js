8cded1b7d5ff5c62595410ecae814b87efb7d471
Alexander Varwijk <git@alexandervarwijk.com>
2020-01-08 22:20:05 +0100

    Split up client/server webpack config
    Apparently we can simply return two webpack config objects to do two
    builds. This allows us to use different configuration for our client
    and server files. The client should run in as many browsers as possible.
    However, the server needs to run on node and does not need any browser
    support.

---
+++ src/server/Promise.js
@@ -0,0 +1,2 @@
+// This is a polyfill that allows webpack to shed most of any-promise.
+module.exports = Promise;
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,65 @@
-module.exports = {
-  entry: {
-    "public/hello": "./src/client/index.js",
-    "server": "./src/server/index.js",
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + "/build",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+const { NormalModuleReplacementPlugin } = require('webpack');
+
+const shared = {
+
+};
+
+module.exports = [
+  {
+    ...shared,
+    entry: "./src/client/index.js",
+    output: {
+      "filename": "hello.js",
+      path: __dirname + "/build/public",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
+      ]
+    }
+  },
+  {
+    ...shared,
+    target: 'node',
+    entry: "./src/server/index.js",
+    output: {
+      filename: 'server.js',
+      path: __dirname + "/build",
+    },
+    plugins: [
+      new NormalModuleReplacementPlugin(/any-promise/, __dirname + '/src/server/Promise.js'),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      node: true,
+                    }
+                  }
+                ]
+              ]
+            }
+          }
+        }
+      ]
+    }
   }
-}
+];
